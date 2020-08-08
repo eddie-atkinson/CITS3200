5601281a90c2af43a65a9599cbf34e51d0796e60
mowemcfc <jcartermcfc@gmail.com>
2020-08-09 04:58:51 +0800

    cypress navbar testing

--- conference-app/tests/e2e/specs/test.js
+++ conference-app/tests/e2e/specs/test.js
@@ -3,6 +3,12 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
-    cy.contains('v-btn', 'Hello vue!');
+    cy.contains('.v-btn__content', 'Hello vue!');
+  });
+
+  it('Navbar Navigation', () => {
+    cy.visit('/');
+    cy.get('.v-btn').contains('About').click();
+    cy.contains('h1', 'This is an about page');
   });
 });
