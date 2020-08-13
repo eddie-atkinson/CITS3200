2c64febb28527844362906a13276880e1512dc0b
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-08-13 17:59:08 +0800

    adds stepper to home page

--- conference-app/src/views/Home.vue
+++ conference-app/src/views/Home.vue
@@ -1,6 +1,45 @@
 <template>
   <div>
     <v-btn class='text-transform uppercase'>Hello vue!</v-btn>
+    <v-stepper v-model='stepNum'>
+      <v-stepper-header>
+        <v-stepper-step
+        :complete="stepNum > 1"
+        step='1'
+        editable='true'
+        edit-icon='$complete'
+        >
+          Step 1
+        </v-stepper-step>
+        <v-stepper-step
+        :complete="stepNum > 2"
+        step='2'
+        editable='true'
+        edit-icon='$complete'
+        >
+          Step 2
+        </v-stepper-step>
+        <v-stepper-step
+        :complete="stepNum > 3"
+        step='3'
+        editable='true'
+        edit-icon='$complete'
+        >
+          Step 3
+        </v-stepper-step>
+      </v-stepper-header>
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          {{ stepNum }}
+        </v-stepper-content>
+        <v-stepper-content step="2">
+          {{ stepNum }}
+        </v-stepper-content>
+        <v-stepper-content step="3">
+          {{ stepNum }}
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
   </div>
 </template>
 
@@ -10,6 +49,11 @@
 export default {
   name: 'Home',
   components: {
+  },
+  data() {
+    return {
+      stepNum: 1,
+    };
   },
 };
 </script>
