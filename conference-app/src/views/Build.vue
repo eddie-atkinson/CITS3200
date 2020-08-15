fbc5d18b043cec039d5be595610b1250ea142510
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-08-15 18:44:17 +0800

    adds basic page structure

--- conference-app/src/views/Build.vue
+++ conference-app/src/views/Build.vue
@@ -1,44 +1,83 @@
 <template>
   <div class="build">
-    <p>hello world</p>
-    <!-- <v-stepper v-model='stepNum'>
-      <v-stepper-header>
-        <v-stepper-step
-        :complete="stepNum > 1"
-        step='1'
-        editable='true'
-        edit-icon='$complete'
+    <v-container>
+      <v-row
+      align='center'
+      justify='center'
+      class='mt-4'
+      >
+        <v-card
+        round
+        class='pa-3 mx-auto'
+        min-width='500'
+        max-width='500'
         >
-          Step 1
-        </v-stepper-step>
-        <v-stepper-step
-        :complete="stepNum > 2"
-        step='2'
-        editable='true'
-        edit-icon='$complete'
-        >
-          Step 2
-        </v-stepper-step>
-        <v-stepper-step
-        :complete="stepNum > 3"
-        step='3'
-        editable='true'
-        edit-icon='$complete'
-        >
-          Step 3
-        </v-stepper-step>
-      </v-stepper-header>
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          {{ stepNum }}
-        </v-stepper-content>
-        <v-stepper-content step="2">
-          {{ stepNum }}
-        </v-stepper-content>
-        <v-stepper-content step="3">
-          {{ stepNum }}
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper> -->
+          <v-card-title>
+            {{ currentTitle }}
+          </v-card-title>
+          <v-window
+          v-model='step'
+          >
+            <v-window-item
+            :value='1'
+            >
+              <v-card-text>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente in tempora doloremque maiores molestiae,
+                ad officia facilis? Fuga, fugit? Dolores ducimus
+                labore rem cupiditate illum harum sunt est dicta laboriosam?
+              </v-card-text>
+            </v-window-item>
+
+            <v-window-item
+            :value='2'
+            >
+              <v-card-text>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Sapiente in tempora doloremque maiores molestiae,
+                ad officia facilis? Fuga, fugit? Dolores ducimus
+                labore rem cupiditate illum harum sunt est dicta laboriosam?
+              </v-card-text>
+            </v-window-item>
+          </v-window>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn
+            :disabled='step === 1'
+            @click='step--'
+            >
+              Previous
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+            :disabled='step === 2'
+            @click='step++'
+            >
+              Next
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
+    </v-container>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      step: 1,
+    };
+  },
+  computed: {
+    currentTitle() {
+      switch (this.step) {
+        case 1: return 'Hello world';
+        case 2: return 'Select your favourite color';
+        default: return 'Need to define the title correctly';
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
