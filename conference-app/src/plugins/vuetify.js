15fdf2c11d6727468197da307afbe6d1b105c80d
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-08-15 17:45:32 +0800

    changes colour scheme

--- conference-app/src/plugins/vuetify.js
+++ conference-app/src/plugins/vuetify.js
@@ -1,7 +1,24 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.teal.base,
+        secondary: colors.indigo.base,
+        accent: colors.blue.base,
+        error: colors.red.lighten1,
+        warning: colors.orange.lighten1,
+        info: colors.indigo.lighten1,
+        success: colors.lime.lighten1,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
+  },
 });
