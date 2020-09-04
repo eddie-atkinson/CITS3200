cdab7f6a3aecdf910bd5a7927c383a7a9c6ad168
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-09-04 09:42:25 +0800

    adds function for converting data into rows for conversion to html

--- conference-app/src/helpers/parse-excel.js
+++ conference-app/src/helpers/parse-excel.js
@@ -1,6 +1,25 @@
 import XLSX from 'xlsx';
 
+function excelToHTML(data) {
+  const wb = XLSX.read(data, {
+    type: 'binary',
+  });
+  const sheetName = wb.SheetNames[0];
+  const ws = wb.Sheets[sheetName];
+  const rowData = XLSX.utils.sheet_to_json(ws);
+  if (rowData.length < 1) {
+    throw new Error('First sheet in Excel document contains no data');
+  }
+  console.log(rowData);
+  const headers = Object.keys(rowData[0]);
+  console.log(headers);
+
+  // TODO call function which converts data into HTML string
+  // return htmlString
+}
+
 function parseExcel(data) {
+  excelToHTML(data);
   return XLSX.read(data, {
     type: 'binary',
   });
@@ -13,4 +32,4 @@ function buildHTML(workbook) {
   });
 }
 
-export { parseExcel, buildHTML };
+export { excelToHTML, parseExcel, buildHTML };
