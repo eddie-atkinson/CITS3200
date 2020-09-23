ebe66b25fc76cd34fce520a6a0ef1cd18bcd4630
Eddie Atkinson <eddie@pop-os.localdomain>
2020-09-23 19:37:54 +0800

    updates styles

--- conference-app/src/helpers/styles.js
+++ conference-app/src/helpers/styles.js
@@ -1,112 +1,62 @@
-const base = `table a:link {
-    color: #666;
-    font-weight: bold;
-    text-decoration:none;
-  }
-  .title {
-    text-transform: capitalize;
-  }
-  .speaker {
-    text-decoration: underline;
-  }
-  table a:visited {
-    color: #999999;
-    font-weight:bold;
-    text-decoration:none;
-  }
-  table a:active,
-  table a:hover {
-    color: #bd5a35;
-    text-decoration:underline;
-  }
-  table {
-    font-family:Arial, Helvetica, sans-serif;
-    color:#666;
-    font-size:12px;
-    text-shadow: 1px 1px 0px #fff;
-    background:#eaebec;
-    margin:20px;
-  }
-  table th {
-    padding:21px 25px 22px 25px;
-
-    background: #ededed;
-    background: -webkit-gradient(linear, left top, left bottom, from(#ededed), to(#ebebeb));
-    background: -moz-linear-gradient(top,  #ededed,  #ebebeb);
-  }
-  table th:first-child {
-    text-align: left;
-    padding-left:20px;
-  }
-  table tr:first-child th:first-child {
-  }
-  table tr:first-child th:last-child {
-  }
-  table tr {
-    text-align: center;
-    padding-left:20px;
-  }
-  table td:first-child {
-    text-align: left;
-    padding-left:20px;
-    border-left: 0;
-  }
-  table td {
-    padding:18px;
-
-    background: #fafafa;
-    background: -webkit-gradient(linear, left top, left bottom, from(#fbfbfb), to(#fafafa));
-    background: -moz-linear-gradient(top,  #fbfbfb,  #fafafa);
-  }
-  table tr.even td {
-    background: #f6f6f6;
-    background: -webkit-gradient(linear, left top, left bottom, from(#f8f8f8), to(#f6f6f6));
-    background: -moz-linear-gradient(top,  #f8f8f8,  #f6f6f6);
-  }
-  table tr:last-child td {
-    border-bottom:0;
-  }
-  table tr:hover td {
-    background: #f2f2f2;
-    background: -webkit-gradient(linear, left top, left bottom, from(#f2f2f2), to(#f0f0f0));
-    background: -moz-linear-gradient(top,  #f2f2f2,  #f0f0f0);
-  }`;
+const base = `
+.center {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+.authors {
+  color: #58595b;
+}
+.title {
+  text-transform: capitalize;
+  padding: 10px;
+  margin: 10px;
+}
+.speaker {
+  text-decoration: underline;
+}
+`;
 
 const orange = `
-table tr:hover td {
-    background: #fee3c6;
-  }
-
-table th {
-    background: #fbb03f;
-}`;
+.break {
+  background-color: #fbb03f;
+  color: #ffffff;
+}
+.plenary {
+  background-color: #fee3c6;
+}
+`;
 
 const blue = `
-table tr:hover td {
-    background: #9fc9eb;
-  }
-
-table th {
-    background: #1b75bc;
-}`;
+.break {
+  background-color: #1b75bc;
+  color: #ffffff;
+}
+.plenary {
+  background-color: #9fc9eb;
+}
+`;
 
 const turq = `
-table tr:hover td {
-    background: #b2b2b2;
-  }
-
-table th {
-    background: #66cccc;
-}`;
+.break {
+  background-color: #643695;
+  color: #ffffff;
+}
+.plenary {
+  background-color: #80deea ;
+}
+`;
 
 const green = `
-table tr:hover td {
-    background: #becca8;
-  }
 
-table th {
-    background: #39b54a;
-}`;
+.break {
+  background-color: #39b54a;
+  color: #ffffff;
+}
+.plenary {
+  background-color: #becca8 ;
+}
+`;
 
 module.exports = {
   base,
